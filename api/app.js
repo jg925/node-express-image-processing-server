472a72a864c3656679ba5bc9df5fb6bf6aff28ab
jg925 <jg925@cornell.edu>
2022-03-02 23:22:33 -0500

    storing image on disk

--- api/app.js
+++ api/app.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 const app = express();
 
+const router = require('./src/router');
+
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
